8cada6e61f855114b86f301ea4a38116a1669c1b
Andy Krohg <akrohg@redhat.com>
2020-02-06 15:27:00 -0500

    Add additional default Shelters and Inclusion Zones

--- src/main/java/com/redhat/emergency/response/disaster/RestApiVerticle.java
+++ src/main/java/com/redhat/emergency/response/disaster/RestApiVerticle.java
@@ -28,7 +28,9 @@ public class RestApiVerticle extends AbstractVerticle {
 
     private static final Logger log = LoggerFactory.getLogger(RestApiVerticle.class);
     private static List<Shelter> shelters = new ArrayList<Shelter>(Arrays.asList(
-        new Shelter("123", "Port City Marina", new BigDecimal(-77.9519), new BigDecimal(34.2461), 0)
+        new Shelter("1", "Port City Marina", new BigDecimal(-77.9519), new BigDecimal(34.2461), 0),
+        new Shelter("2", "Wilmington Marine Center", new BigDecimal(-77.949), new BigDecimal(34.1706), 0),
+        new Shelter("3", "Carolina Beach Yacht Club", new BigDecimal(-77.8885), new BigDecimal(34.0583), 0)
     ));
     private static DisasterCenter disasterCenter = new DisasterCenter("Wilmington, North Carolina, United States", 
         new BigDecimal(-77.886765), new BigDecimal(34.158808));
@@ -51,6 +53,30 @@ public class RestApiVerticle extends AbstractVerticle {
 			new double[]{-77.937, 34.151},
 			new double[]{-77.954, 34.190},
 			new double[]{-77.95, 34.26}
+        )),
+        new InclusionZone("2", Arrays.asList(
+            new double[]{-77.981, 34.232},
+			new double[]{-77.954, 34.227},
+			new double[]{-77.965, 34.207},
+			new double[]{-77.964, 34.183},
+			new double[]{-77.967, 34.184},
+			new double[]{-77.971, 34.204},
+            new double[]{-77.975, 34.221},
+            new double[]{-77.981, 34.232}
+        )),
+        new InclusionZone("3", Arrays.asList(
+            new double[]{-77.943, 34.130},
+			new double[]{-77.940, 34.128},
+			new double[]{-77.939, 34.120},
+			new double[]{-77.938, 34.117},
+			new double[]{-77.941, 34.112},
+			new double[]{-77.944, 34.116},
+			new double[]{-77.947, 34.117},
+			new double[]{-77.948, 34.122},
+			new double[]{-77.946, 34.124},
+			new double[]{-77.942, 34.123},
+            new double[]{-77.943, 34.128},
+            new double[]{-77.943, 34.130}
         ))
     ));
 
